b25a1cf16ac326b78febb63fdd36a08e1e52879b
xljiulang <366193849@qq.com>
2017-07-29 00:47:13 +0800

    MqttException error message

--- Paho.MqttDonet/MqttError.cs
+++ Paho.MqttDonet/MqttError.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,66 +15,79 @@ namespace Paho.MqttDotnet
         /// <summary>
         /// 成功
         /// </summary>
+        [Description("成功")]
         Success = 0,
 
         /// <summary>
         /// 失败
         /// </summary>
+        [Description("失败")]
         Failure = -1,
 
         /// <summary>
         /// 持久化错误
         /// </summary>
+        [Description("持久化错误")]
         PersistenceError = -2,
 
         /// <summary>
         /// 连接已断开 
         /// </summary>
+        [Description("连接已断开")]
         Disconnected = -3,
 
         /// <summary>
         /// 超过最大消息飞行窗口
         /// </summary>
+        [Description("超过最大消息飞行窗口")]
         MaxMessagesInflight = -4,
 
         /// <summary>
         /// 无效的utf8字符串
         /// </summary>
+        [Description("无效的utf8字符串")]
         BadUtf8String = -5,
 
         /// <summary>
         /// 参数为NULL
         /// </summary>
+        [Description("参数为NULL")]
         NullParameter = -6,
 
         /// <summary>
         /// 主题被截断
         /// </summary>
+        [Description("主题被截断")]
         TopicnameTruncated = -7,
 
         /// <summary>
         /// 结构体版本号或id问题
         /// </summary>
+        [Description("结构体版本号或id问题")]
         BadStructure = -8,
 
         /// <summary>
         /// qos无效
         /// </summary>
+        [Description("qos无效")]
         BadQos = -9,
 
         /// <summary>
         /// 无更多的消息id可以使用
         /// </summary>
+        [Description("无更多的消息id可以使用")]
         NoMoreMsgids = -10,
 
         /// <summary>
         /// 操作未完成
         /// </summary>
+        [Description("操作未完成")]
         OperationIncomplete = -11,
 
         /// <summary>
         /// 消息过大
         /// </summary>
+        [Description("消息过大")]
         MaxBufferedMessages = -12,
     }
 }
--- Paho.MqttDonet/MqttException.cs
+++ Paho.MqttDonet/MqttException.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
 
 namespace Paho.MqttDotnet
 {
@@ -12,21 +14,64 @@ namespace Paho.MqttDotnet
     public class MqttException : Exception
     {
         /// <summary>
-        /// mqtt客户端异常
+        /// 保存错误码的提示消息
         /// </summary>
-        /// <param name="error">异常码</param>
-        public MqttException(MqttError error) :
-            base(error.ToString())
+        private static readonly IDictionary<MqttError, string> errorMessages;
+
+        /// <summary>
+        /// 获取mqtt错误码
+        /// </summary>
+        public MqttError Error { get; private set; }
+
+        /// <summary>
+        /// 静态构造器
+        /// </summary>
+        static MqttException()
         {
+            var type = typeof(MqttError);
+            var q = from e in Enum.GetValues(type).Cast<MqttError>()
+                    let field = type.GetField(e.ToString())
+                    let attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute
+                    select new { key = e, value = attribute.Description };
+
+            MqttException.errorMessages = q.ToDictionary(kv => kv.key, kv => kv.value);
         }
 
         /// <summary>
         /// mqtt客户端异常
         /// </summary>
         /// <param name="error">异常码</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public MqttException(MqttError error) :
+            base(MqttException.GetEerrorMessage(error))
+        {
+            this.Error = error;
+        }
+
+        /// <summary>
+        /// mqtt客户端异常
+        /// </summary>
+        /// <param name="error">异常码</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public MqttException(int error) :
             this((MqttError)error)
         {
+        }
+
+        /// <summary>
+        /// 获取错误信息
+        /// </summary>
+        /// <param name="error"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <returns></returns>
+        private static string GetEerrorMessage(MqttError error)
+        {
+            var message = default(string);
+            if (MqttException.errorMessages.TryGetValue(error, out message) == false)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return message;
         }
     }
 }
